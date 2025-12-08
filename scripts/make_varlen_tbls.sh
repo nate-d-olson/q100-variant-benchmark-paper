e761bc20403ddbf13bfcc9b4f3c58a6c087c3b24
nate-d-olson <nolson@nist.gov>
2025-12-08 15:33:36 -0500

    incorporated into snakemake pipeline

--- scripts/make_varlen_tbls.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-VCF=$1
-BED=$2
-
- bcftools query -HH \
-            -f '%CHROM\t%POS0\t%END\t%SVTYPE\t%VKX\t%SVLEN\t%TRF\t%TRFstart\t%TRFend\t%RM_clsfam\n' \
-            -R ${BED} ${VCF}
